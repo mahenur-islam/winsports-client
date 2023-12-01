830f1bd9339a6650b473d835083393047116f6c0
Mahenur Islam <mahenurislam97@gmail.com>
2023-12-02 02:41:24 +0600

    Blog Details Page Created

--- src/components/BlogDetails.js/BlogDetails.jsx
+++ src/components/BlogDetails.js/BlogDetails.jsx
@@ -1,17 +1,32 @@
-// BlogDetails.js
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const BlogDetails = () => {
   const blog = useLoaderData();
 
   return (
-    <div>
-      <h1>{blog.title}</h1>
-      <p>{blog.shortDescription}</p>
-      <p>{blog.content}</p>
+    <div className="w-2/3 mx-auto text-justify mt-10 shadow-2xl shadow-gray-400 p-10">
+      <div>
+        <h1 className="text-2xl md:text-3xl font-bold mb-10 text-[#053B50] p-3">
+          {blog.title}
+        </h1>
+      </div>
+      <div className="">
+        <img src={blog.image} className="rounded-lg mb-5" />
+        <h1 className=" bg-green-300 inline rounded-md p-2 font-semibold text-orange-500">{blog.category}</h1>
+      </div>
+      <div>
+        <h4 className="mt-10 mb-5 text-lg text-left font-semibold text-gray-700">{blog.shortDescription}</h4>
+        <h1 className="md:text-xl text-gray-700 pb-20">{blog.details}</h1>
+      </div>
     </div>
   );
 };
 
 export default BlogDetails;
+
+{
+  /* <h1>{blog.title}</h1>
+      <p>{blog.shortDescription}</p>
+      <p>{blog.details}</p> */
+}
