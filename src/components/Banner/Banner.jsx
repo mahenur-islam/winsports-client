46e17880aca9418a3801860d0136a2561017222e
Mahenur Islam <mahenurislam97@gmail.com>
2023-12-04 13:34:47 +0600

    banner image changed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,27 +6,22 @@ const Banner = () => {
         <div className="h-[300px] md:h-[500px] lg:h-[600px] relative">
             <Carousel slideInterval={8000}>
                 <div className="relative">
-                    <img src="https://i.ibb.co/zSqCCsw/Untitled-design-7.png" alt="..." className='w-full'/>
-                    {/* <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white">
-                        <h1 className="text-3xl font-bold">This is carousel1</h1>
-                        <p className="text-lg">Your carousel text goes here.</p>
-                        <Button className="mt-4">Learn More</Button>
-                    </div> */}
+                    <img src="hhttps://i.ibb.co/7C7wqTc/pexels-mike-1171084.jpg" alt="..." className='w-full'/>
                 </div>
                 <div className="relative">
-                    <img src="https://i.ibb.co/rcY2Jhs/pexels-bono-tsang-7932148.jpg " alt="..." className='w-full'/>
+                    <img src="https://i.ibb.co/rx8qRyt/pexels-tembela-bohle-1884574.jpg" alt="..." className='w-full'/>
                 </div>
                 <div className="relative">
-                    <img src="https://i.ibb.co/94VsSs5/1.png" alt="..." className='w-full'/>
+                    <img src="https://i.ibb.co/QNTwGJ8/pexels-roger-lagesse-2745827.jpg" alt="..." className='w-full'/>
                 </div>
                 <div className="relative">
-                    <img src="https://i.ibb.co/jyFHn2q/2.png" alt="..." className='w-full'/>
+                    <img src="https://i.ibb.co/cDQyBsb/Tennis-4-min.jpg" alt="..." className='w-full'/>
                 </div>
                 <div className="relative">
-                    <img src="https://i.ibb.co/qynqrtx/3.png" alt="..." className='w-full'/>
+                    <img src="https://i.ibb.co/NNZCthV/Rugby-1-min.jpg" alt="..." className='w-full'/>
                 </div>
                 <div className="relative">
-                    <img src="https://i.ibb.co/DrnYjgT/BANGLADESH.png" alt="..." className='w-full'/>
+                    <img src="https://i.ibb.co/6BY7gDF/Boxing-1-min.jpg" alt="..." className='w-full'/>
                 </div>
             </Carousel>
         </div>
